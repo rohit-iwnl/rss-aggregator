fb78495b9a4ce5c32dcb3f3ab51a51720dd9a610
Rohit Manivel <rohitmanivel9@gmail.com>
2024-02-16 17:35:32 -0700

    add chi router and http server

--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main(){
-	fmt.Println("Hello, World!")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if(portString == ""){
+		log.Fatal("PORT is not set in .env file")
+	}
+	fmt.Println("PORT is set to", portString);
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	srv := &http.Server{
+		Handler : router,
+		Addr: ":" + portString,
+	}
+	log.Printf("Server is running on port %s", portString)
+	err := srv.ListenAndServe()
+	if err!=nil{
+		log.Fatal(err)
+	}
 }
