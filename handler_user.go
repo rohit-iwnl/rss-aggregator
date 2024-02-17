721e54b95f110c25df2d1a6115c189e44095f70e
Rohit Manivel <rohitmanivel9@gmail.com>
2024-02-16 18:57:42 -0700

    added create user endpoint

---
+++ handler_user.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/rohit-iwnl/rss-aggregator/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request){
+	type parameters struct {
+		Name string `json:"name"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Invalid request payload: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID : uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name : params.Name,
+
+		 
+	})
+
+	if err!=nil{
+		respondWithError(w,400,fmt.Sprintf("Cannot create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w,200,user)
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,14 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	"github.com/rohit-iwnl/rss-aggregator/internal/database"
+	_ "github.com/lib/pq"
+
 )
+
+type apiConfig struct{
+	DB *database.Queries
+}
 
 func main(){
 	godotenv.Load(".env")
@@ -19,6 +27,23 @@ func main(){
 		log.Fatal("PORT is not set in .env file")
 	}
 	fmt.Println("PORT is set to", portString);
+
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DATABASE_URL is not set in .env file")
+	}
+
+	conn,err := sql.Open("postgres", dbUrl)
+
+	if err!=nil{
+		log.Fatal("Cannot connect to database", err)
+	}
+
+
+	
+	apiCfg := apiConfig{
+		DB : database.New(conn),
+	}
 
 	router := chi.NewRouter()
 
@@ -35,6 +60,7 @@ func main(){
 
 	v1Router.Get("/ready", http.HandlerFunc(handlerReadiness))
 	v1Router.Get("/error", http.HandlerFunc(handleError))
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -43,7 +69,7 @@ func main(){
 		Addr: ":" + portString,
 	}
 	log.Printf("Server is running on port %s", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err!=nil{
 		log.Fatal(err)
 	}
